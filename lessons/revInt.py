d51b933a8dc6741977c5f677c40c8b6933c99351
AngryKiwi <129200338+VeryAngrykiwi@users.noreply.github.com>
2024-03-20 10:56:04 +0000

    revInt add

---
+++ lessons/revInt.py
@@ -0,0 +1,34 @@
+num = input('Введите число:')
+print(int(num[:-5] + num[-5:][::-1]))
+
+'''
+num = input()
+if len(num) == 5:
+    print(int(num[-1::-1]))
+else:
+    print(int(num[0] + num[-1:-6:-1]))
+
+a = input()
+d = 0
+b = str()
+c = str()
+if len(a) == 6:
+    b = a[:0:-1]
+    c = a[0] + b
+    d = int(c)
+    print(d)
+if len(a) == 5:
+    b = a[::-1]
+    if b[-1] != 0:
+        d = int(b)
+        print(d)
+    else:
+        for i in range(len(a)):
+            if b[i] != '0':
+                c += b[i]
+        d = int(c)
+    print(c)
+
+(lambda x: print(int(x[:-5] + x[-5:][::-1])))(input())
+
+'''
