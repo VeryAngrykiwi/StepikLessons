4554f0a32951590e53c06d916911c2255912c0de
AngryKiwi <129200338+VeryAngrykiwi@users.noreply.github.com>
2024-04-04 14:13:29 +0000

    Maximum in area add ask and solution

--- lessons/Maximum in area.py
+++ lessons/Maximum in area.py
@@ -1,3 +1,5 @@
+#maximum in area 1
+
 n = int(input())
 matrix = [list(map(int, input().split())) for _ in range(n)]
 
@@ -66,4 +68,104 @@ for x in range(n):
         elif x > j:  # если x > j, то добавяем в список result
             result.append(matrix[x][j])
 print(max(result))  # выводим на экран
+'''
+
+#maximum in area 2
+
+n = int(input())
+matrix = []
+for k in range(n):
+    tmp = [int(x) for x in input().split()]
+    matrix.append(tmp)
+
+max = matrix[0][0]
+
+for i in range(n):
+    for j in range(n):
+        if (i >= j and i <= n - 1 - j or i <= j and i >= n - 1 - j) and matrix[i][j] > max:
+            max = matrix[i][j]
+
+print(max)
+
+'''
+n = int(input())
+matrix = []
+
+for _ in range(n):
+    row = [int(i) for i in input().split()]
+    matrix.append(row)
+
+largest = matrix[0][0]
+
+for i in range(n):
+    for j in range(n):
+        if (i >= j and i + j + 1 <= n) or (i <= j and i + j + 1 >= n):
+            if matrix[i][j] > largest:
+                largest = matrix[i][j]
+
+print(largest)
+
+
+
+n = int(input())
+m = [list(map(int, input().split())) for _ in range(n)]
+print(max(max(m[i][j], m[i][~j], m[~i][j], m[~i][~j]) for i in range(n // 2 + 1) for j in range(i + 1)))
+
+
+
+maximum = []
+m = int(input())
+for i in range(m):
+    my_string = [int(i) for i in input().split()]
+    maximum.extend(my_string[:min(i + 1, m - i)])
+    maximum.extend(my_string[max(-i - 1, i - m):])
+print(max(maximum))
+
+
+
+# обработка вводимых данных и преобразование их в квадратную матрицу
+def matrix_sqr():
+    matrix = [input().split() for _ in range(int(input()))]
+    return matrix
+# нахождение эл-тов заштрихованной части квадратной матрицы
+def stroke_matrix_sqr(matrix): 
+    nums = []
+    # добавляем эл-ты главной и побочной диагоналей
+    for i in range(len(matrix)):
+        nums.append(int(matrix[i][i]))
+        nums.append(int(matrix[i][len(matrix)-i-1]))
+    # добавляем заштрихованные части матрицы
+    for j in range(len(matrix[0])):
+        for i in range(len(matrix)):
+            if (j<i and i+j+1< len(matrix)) or (i<j and i+j+1> len(matrix)):
+                nums.append (int(matrix[i][j]))
+    # выводим максимум из заштрихованной части            
+    return max(nums)
+
+matrix = matrix_sqr()
+print (stroke_matrix_sqr(matrix))
+
+
+n = int(input())
+a = [[*map(int, input().split())] for _ in range(n)]
+print(max(max(a[i][j], a[i][~j]) for j in range(n // 2) for i in range(j, n - j)))
+
+
+размерность = int(input())
+диагонали = tuple(range(1,размерность//2+1))
+диагонали = диагонали + ((размерность//2+1,) if размерность%2 else ()) + диагонали[::-1]
+
+взять_линию = lambda: (*map(int, input().split()),)
+матрица = *(взять_линию() for _ in range(размерность)),
+
+бабочка = []
+for позиция, линия in zip(диагонали, матрица):    
+    убрать = slice(позиция, -позиция)
+    линия = list(линия)
+    линия[убрать] = []
+
+    добавить = slice(len(бабочка),0)
+    бабочка[добавить] = линия
+
+print( max(бабочка) )
 '''
