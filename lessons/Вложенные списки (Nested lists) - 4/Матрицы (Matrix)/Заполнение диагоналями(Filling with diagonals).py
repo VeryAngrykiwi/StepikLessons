dbd730b92e348a75792ca3864e76016c883abf24
AngryKiwi <ozzlandia@yandex.ru>
2024-05-15 15:48:03 +0300

    Filling with diagonals add

---
+++ lessons/Вложенные списки (Nested lists) - 4/Матрицы (Matrix)/Заполнение диагоналями(Filling with diagonals).py
@@ -0,0 +1,215 @@
+# Вариант 1:
+n, m = map(int, input().split())
+mtx = [[0 for _ in range(m)] for _ in range(n)]
+
+num = 1
+for j in range(m+n-1):
+    for i in range(n):
+        if j - i in range(m):
+            mtx[i][j - i] = num
+            num += 1
+
+for i in range (n):
+    for j in range(m):
+        print(str(mtx[i][j]).ljust(2), end= ' ')
+    print()
+
+# Вариант 2:
+# вход строка чисел
+x = input().split()
+
+# считывание строки чисел
+n, m = int(x[0]), int(x[1])
+
+# создание пустой матрицы
+mtx = []
+
+# построение матрицы методом append, наполнение условным объектом e.
+for _ in range(n):
+    tmp = [e for e in range(m)]  # временная переменная наполняющая матрицу (list comprehension,генератор списков)
+    mtx.append(tmp)  # наполнение матрицы
+
+# наполнение матрицы числами в соответствии с условием
+num = 1 # счетчик
+for k in range(n * m):  # размер матрицы, цикл по всей матрице
+    for i in range(n):  # цикл по строкам
+        for j in range(m):  # цикл по столбцам
+            if i + j == k:  # условие сумма индексов (столбец) по диагонали.
+                mtx[i][j] = num
+                num += 1
+
+# печать матрицы с отступами, методом str.
+for i in range(n):
+    for j in range(m):
+        print(str(mtx[i][j]).ljust(2), end=' ')
+    print()
+
+'''
+n, m = [int(el) for el in input().split()]
+matrix = [[None for _ in range(m)] for _ in range(n)]
+cnt = 1
+
+# проходим по всем диагоналям
+for d in range(n + m - 1):
+    for i in range(n):
+        for j in range(m):
+            if i + j == d:
+                matrix[i][j] = cnt
+                cnt += 1
+
+for i in range(n):
+    for j in range(m):
+        print(str(matrix[i][j]).ljust(3), end="")
+    print()
+    
+
+
+n, m = [int(i) for i in input().split()]
+
+matrix = [[1] * m for _ in range(n)]
+row, col, diag = 0, 0, 0
+
+for i in range(1, n * m):
+    col -= 1
+    row += 1
+    if col < 0 or row == n:
+        diag += 1
+        col = diag if diag < m else m - 1
+        row = diag - col
+    
+    matrix[row][col] += i
+
+[print(*i) for i in matrix]
+
+
+
+n, m = map(int, input().split())
+mt = [[''] * m for i in '1'* n]
+d = 1
+for k in range(1, n + m + 1):
+    for i in range(n):
+        for j in range(m):
+            if i + j + 1 == k:
+                mt[i][j] = str(d).ljust(3)
+                d += 1
+[print(*r, sep='') for r in mt]
+
+
+
+# Принимаем параметры матрицы
+n, m = map(int, input().split())
+# Создаем скелет матрицы
+matrix = [[0] * m for i in range(n)]
+# Задаем отсчет с единицы
+d = 1
+
+for k in range(1, n + m):               # Цикл перебирающий сумму индексов в диагонали
+    for i in range(n):                  # Перебираем строки
+        for j in range(m):              # Перебираем столбцы
+            if i + j + 1 == k:          # Выявляем ячейки, относящиеся к искомой диагонали
+                matrix[i][j] = d        # Присваиваем обнаруженной ячейке порядковый номер
+                d += 1                  # Обновляем счетчик
+
+# Распечатываем полученную матрицу
+for row in range(n):
+    for col in range(m):
+        print(str(matrix[row][col]).ljust(3), end=' ')
+    print()
+
+
+
+(n, m), cnt = map(int, input().split()), 1
+a = [[0] * m for _ in range(n)]
+for d in range(m + n - 1):
+    for i in range(max(0, d - m + 1), min(d, n - 1) + 1):
+        a[i][d - i], cnt = cnt, cnt + 1
+for row in a:
+    print(*map(lambda x: f'{x:<2}', row))
+    
+   
+
+[n, m] = [int(i) for i in input().split()]
+matrix = [['0']*m for _ in range(n)] 
+num = 0
+
+for d in range(m + n - 1):
+    for i in range(n):
+        j = d - i
+        if 0 <= i < n and 0 <= j < m:
+            num += 1
+            matrix[i][j] = str(num).ljust(3)
+            
+
+for i in range(n):
+    print(*matrix[i])
+    
+
+list = input().split()
+a = int(list[0])
+b = int(list[1])
+matrix = []
+elem = []
+total = 0
+
+if a > b:
+    for i in range(a):
+        for k in range(b):
+            diagonals = i + k + 1
+            old_diagonals = diagonals - 1
+            for h in range(1, old_diagonals + 1):
+                if h < b:
+                    total += h
+                elif b <= h <= a:
+                    total += b
+                elif h > a:
+                    total += b - (h - a)
+            if diagonals <= b:
+                total += i + 1
+            elif diagonals > b:
+                total += (i + 1) - (diagonals - b)
+            elem.append(total)
+            total = 0
+        matrix.append(elem)
+        elem = []
+elif b > a:
+    for i in range(a):
+        for k in range(b):
+            diagonals = i + k + 1
+            old_diagonals = diagonals - 1
+            for h in range(1, old_diagonals + 1):
+                if h < a:
+                    total += h
+                elif a <= h <= b:
+                    total += a
+                elif h > b:
+                    total += a - (h - b)
+            if diagonals <= b:
+                total += i + 1
+            elif diagonals > b:
+                total += (i + 1) - (diagonals - b)
+            elem.append(total)
+            total = 0
+        matrix.append(elem)
+        elem = []
+elif a == b:
+    for i in range(a):
+        for k in range(b):
+            diagonals = i + k + 1
+            old_diagonals = diagonals - 1
+            for h in range(1, old_diagonals + 1):
+                if h <= a:
+                    total += h
+                elif h > b:
+                    total += b - (h - b)
+            if diagonals <= b:
+                total += i + 1
+            elif diagonals > b:
+                total += (i + 1) - (diagonals - b)
+            elem.append(total)
+            total = 0
+        matrix.append(elem)
+        elem = []
+
+for i in range(a):
+    print(*matrix[i])
+'''
