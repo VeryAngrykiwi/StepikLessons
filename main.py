748310efaecb43740e292eb8305df01e16ccf911
AngryKiwi <129200338+VeryAngrykiwi@users.noreply.github.com>
2024-05-20 14:01:28 +0000

    sync

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-n = int(input())
-
-mtx = [] #матрица - пустой список 
-for _ in range(n): #цикл наполнения матрицы
-  tmp = [0 for _ in range(n)]
-  mtx.append(tmp)
-
-
-for i in range(n): 
-  for q in range(n):
-    if i == q:
-      mtx[i][q] = 1
-    elif i == n-1-q:
-      mtx[i][q] = 1
-
-[print(*row) for row in mtx]
-print()
-[print(*mtx[i]) for i in range(n)]
 
 
 
