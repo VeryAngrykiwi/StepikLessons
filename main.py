6668470d0e739a6456e4e62904d885db176b476b
AngryKiwi <129200338+VeryAngrykiwi@users.noreply.github.com>
2024-06-03 11:02:06 +0000

    sync

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
-n, m, k, x, y, z, t, a = [int(input()) for _ in range(8)]
-
-two_books1_2 = n + m - x - t
-two_books2_3 = m + k - y - t
-two_books1_3 = n + k - z - t
-two_books_read = two_books1_2 + two_books2_3 + two_books1_3
-
-read_book1_only = n - two_books1_2 - two_books1_3 - t
-read_book2_only = m - two_books1_2 - two_books2_3 - t
-read_book3_only = k - two_books2_3 - two_books1_3 - t
-one_book_only = read_book1_only + read_book2_only + read_book3_only
-
-no_read = a - one_book_only - two_books_read - t
-print(one_book_only, two_books_read, no_read, sep='\n')
+myset = set([1, 2, 3, '1', '2', '3'])
+print(len(myset))
